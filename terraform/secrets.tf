f11aa771328dfd1ff2fc8617bd8d37278a0f9405
ryaan-anthony <rtulino@gmail.com>
2020-04-06 14:04:17 -0400

    update secrets

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,9 +1,9 @@
-resource "aws_secretsmanager_secret" "deploy_key_pair" {
-  name = "${var.project}-${var.environment}-deploy_key_pair"
+resource "aws_secretsmanager_secret" "credentials" {
+  name = "${var.project}-${var.environment}-credentials"
 }
 
 resource "aws_secretsmanager_secret_version" "deploy_key_pair" {
-  secret_id     = aws_secretsmanager_secret.deploy_key_pair.id
+  secret_id     = aws_secretsmanager_secret.credentials.id
   secret_string = jsonencode({
     user = var.instance_user
     private_key_pem = tls_private_key.deploy_key_pair.private_key_pem
